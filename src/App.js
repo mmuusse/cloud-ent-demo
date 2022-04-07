cf585fa414702a6ccb6b40664d7f082f03f6d09b
Melyssa Muusse <melyssamuusse@gmail.com>
2022-04-07 11:53:39 -0700

    updating logo and background color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo-navy.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HB ENT-FULL onboarding example
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
